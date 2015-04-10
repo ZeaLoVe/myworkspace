78bf6d29f150e60d0f224555d276bdfb0d2c50c3
ZeaLoVe <ruanguiliang_34@whu.edu.cn>
2015-04-10 19:18:31 +0800

    modify

--- sdagent/job.go
+++ sdagent/job.go
@@ -3,6 +3,11 @@ package sdagent
 import ()
 
 type Job struct {
-	r *Register
-	s *Service
+	r        *Register
+	s        *Service
+	stopChan chan uint64 //check whether job is down
+}
+
+func (j *Job) Run() {
+
 }
--- sdagent/sdagent.go
+++ sdagent/sdagent.go
@@ -1,9 +1,26 @@
 package sdagent
 
-import ()
+import (
 	"log"
+)
 
 const Version = "0.1"
+
+type SDAgent struct {
+	jobs []Job
+}
+
+func (sda *SDAgent) Run() {
+
+}
+
+func (sda *SDAgent) AddJob() {
+
+}
+
+func (sda *SDAgent) DeleteJob() {
+
+}
 
 func main() {
 
