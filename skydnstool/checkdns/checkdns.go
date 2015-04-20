b7404d39cd0ea335791b97042ab9ab56b4bcdd08
ZeaLoVe <ruanguiliang_34@whu.edu.cn>
2015-04-20 11:26:52 +0800

    fixed result check error

--- skydnstool/checkdns/checkdns.go
+++ skydnstool/checkdns/checkdns.go
@@ -31,6 +31,18 @@ type Worker struct {
 	stopChan         chan int
 }
 
+func ShowCheckMap() {
+	for key, value := range CheckMap {
+		fmt.Println(key + ":" + value)
+	}
+}
+
+func ShowCheckList() {
+	for _, str := range ReqList {
+		fmt.Println(str)
+	}
+}
+
 func (w *Worker) DoRequest() {
 	w.ReqSendNum = w.ReqSendNum + 1
 	req := ReqList[w.ReqSendNum%w.lenOfRequestList]
@@ -43,8 +55,8 @@ func (w *Worker) DoRequest() {
 		}
 	} else {
 		for _, ip := range ips {
-			if CheckMap[ip] == req {
-				//fmt.Printf("CheckMap of %v:%v  req:%v\n", ip, CheckMap[ip], req)
+			//fmt.Printf("CheckMap of corretct ip:%v  Got ip:%v\n", CheckMap[req], ip)
+			if CheckMap[req] == ip {
 				w.ResSuccessNum = w.ResSuccessNum + 1
 				return
 			}
@@ -62,9 +74,9 @@ func (w *Worker) Run() {
 }
 
 func main() {
-	flag.IntVar(&QPS, "q", 100, "-q to set qps per thread")
+	flag.IntVar(&QPS, "q", 10, "-q to set qps per thread")
 	flag.IntVar(&ThreadNum, "t", 5, "-t to set worker thread,default set as 5")
-	flag.IntVar(&RequestATread, "r", 100, "-r to set the nums of request a worker will send ,default as 10000")
+	flag.IntVar(&RequestATread, "r", 1000, "-r to set the nums of request a worker will send ,default as 1000")
 	flag.BoolVar(&CheckRes, "c", false, "-c to set whether check the result,default not")
 	flag.StringVar(&CheckFile, "f", "", "-f to set check file, DNS(name ip) sets")
 	flag.Parse()
@@ -91,9 +103,9 @@ func main() {
 				strs := strings.Split(line, " ")
 				if len(strs) != 2 {
 					fmt.Println("format error ,must be:service address")
-					break
+					continue
 				}
-				CheckMap[strs[1]] = strs[0]        //make check map
+				CheckMap[strs[0]] = strs[1]        //make check map
 				ReqList = append(ReqList, strs[0]) //make request list
 				if err == io.EOF {
 					break
@@ -101,6 +113,9 @@ func main() {
 			}
 		}
 	}
+	//ShowCheckList()
+	//ShowCheckMap()
+
 	for i := 0; i < ThreadNum; i++ {
 		worker := new(Worker)
 		workers = append(workers, *worker)
@@ -108,7 +123,7 @@ func main() {
 	for i := 0; i < ThreadNum; i++ {
 		sleeptime := float64(1000) / float64(QPS)
 		workers[i].interval = time.Duration(sleeptime) * time.Millisecond
-		fmt.Printf("threa ID:%v  internval: %v\n", i, workers[i].interval)
+		fmt.Printf("thread ID:%v  internval: %v\n", i, workers[i].interval)
 		workers[i].lenOfRequestList = len(ReqList)
 		workers[i].ReqSendNum = 0
 		workers[i].ResFailNum = 0
@@ -128,6 +143,6 @@ func main() {
 		ResSuccessSum += workers[i].ResSuccessNum
 	}
 	fmt.Printf("Totally send %v request,Get %v, failed %v,success %v\n", ThreadNum*RequestATread, ResFailSum+ResSuccessSum, ResFailSum, ResSuccessSum)
-	fmt.Printf("Usable rate: %v\n", float64(ResSuccessSum)/(float64(ThreadNum*RequestATread)))
+	fmt.Printf("Usable rate: %f\n", float64(ResSuccessSum)/(float64(ThreadNum*RequestATread)))
 
 }
